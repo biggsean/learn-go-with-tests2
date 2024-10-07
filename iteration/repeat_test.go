23ff285f443d9571408eec1d8b1b895aef847a2a
Sean McGowan <spm@cringer.home.mcgowanet.com>
2024-10-06 21:35:08 -0400

    Chapter 3: Iteration
    Use strings package and provide example

---
+++ iteration/repeat_test.go
@@ -0,0 +1,36 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestRepeatMore(t *testing.T) {
+	repeated := Repeat("a", 7)
+	expected := "aaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 10)
+	}
+}
+
+func ExampleRepeat() {
+	str := Repeat("b", 10)
+	fmt.Println(str)
+	// Output: bbbbbbbbbb
+}
